1eaac3ea918164106cc7bc315d7f1c0c6678bcb1
michaelchesters27-pixel <michaelchesters27@gmail.com>
2026-08-25 00:26:07 +0100

    Add live Value Engine dashboard to scanner

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ValueDashboard from './ValueDashboard'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ValueDashboard />
   </React.StrictMode>,
 )
