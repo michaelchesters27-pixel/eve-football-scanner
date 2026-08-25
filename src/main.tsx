3699a748976770c1c8bd892e1f9217b2afe6c316
michaelchesters27-pixel <michaelchesters27@gmail.com>
2026-08-25 20:20:06 +0100

    Use multi-page scanner shell and expansion styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ValueDashboard from './ValueDashboard'
 import './styles.css'
+import './expansion.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ValueDashboard />
   </React.StrictMode>,
 )
