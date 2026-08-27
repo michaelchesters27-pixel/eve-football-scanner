84fbd88ee7c49ecccc9faa430b7b8f64ceb3df59
michaelchesters27-pixel <michaelchesters27@gmail.com>
2026-08-28 00:14:14 +0100

    Route scanner results page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ResultsPage from './ResultsPage'
 import './styles.css'
 import './expansion.css'
+import './results.css'
+
+function Root(){
+  const isResults=()=>window.location.hash.replace(/^#\/?/,'')==='results'
+  const [results,setResults]=useState(isResults)
+
+  useEffect(()=>{
+    const onHash=()=>setResults(isResults())
+    window.addEventListener('hashchange',onHash)
+    return()=>window.removeEventListener('hashchange',onHash)
+  },[])
+
+  if(results)return <ResultsPage/>
+  return <><App/><a className="results-shortcut" href="#/results">Results</a></>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
